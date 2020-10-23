b51caa1735e4bd879a0e5c33c658e2c9a1562871
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-10-23 23:36:45 +0200

    use UsersRepo insted of UsersMoel

--- src/MiddleWares/Auth.php
+++ src/MiddleWares/Auth.php
@@ -2,73 +2,75 @@
 
 namespace SM\MiddleWares;
 
+use config\DBConfig;
+use Simple\Core\DataAccess\MySQLAccess;
 use Simple\Core\Request;
-use Simple\Helpers\Session;
-use SM\Models\Users\UserModel;
+use Simple\Core\Session;
+use SM\Repos\Users\UsersRepo;
 
 class Auth
 {
-    private static array $usersTypes = [
-        'admin',
-        'exams',
-        'student',
-        'employee',
-        'employeesAffairs',
-        'studentAffairs'
-    ];
+  private static array $usersTypes = [
+    'admin',
+    'exams',
+    'student',
+    'employee',
+    'employeesAffairs',
+    'studentAffairs'
+  ];
 
-    public function isAuthenticated()
-    {
+  public function isAuthenticated()
+  {
+    Session::start();
+    return Session::exists('userId');
+  }
+  /**
+   * Check if the user is logged in
+   * @param Simple\Core\Request $request the request object
+   * @return bool
+   */
+  public static function authenticate(Request $request)
+  {
+    $params = $request->getRequestBody()['post'];
+    $userName = isset($params['userName']) && !empty($params['userName']) ? $params['userName'] : false;
+    $password = isset($params['password']) && !empty($params['password']) ? $params['password'] : false;
+
+    if (!$userName) {
+      exit('!! EMPTY USER NAME NOT ALLOWED !!');
+    }
+
+    if (!$password) {
+      exit('!! EMPTY PASSWORD NOT ALLOWED !!');
+    }
+
+    $repo = new UsersRepo(new MySQLAccess(new DBConfig()));
+    $data = $repo->auth($userName, $password);
+
+    if ($data) {
+      foreach ($data as $key => $value) {
         Session::start();
-        return Session::exists('userId');
+        Session::set($key, $value);
+      }
+      Session::set('userType', self::$usersTypes[$data['groupId']]);
+      return true;
     }
-    /**
-     * Check if the user is logged in
-     * @param Simple\Core\Request $request the request object
-     * @return bool
-     */
-    public static function authenticate(Request $request)
-    {
-        $params = $request->getRequestBody()['post'];
-        $userName = isset($params['userName']) && !empty($params['userName']) ? $params['userName'] : false;
-        $password = isset($params['password']) && !empty($params['password']) ? $params['password'] : false;
+    return false;
+  }
 
-        if (!$userName) {
-            exit('!! EMPTY USER NAME NOT ALLOWED !!');
-        }
+  /**
+   * Authorize the logged in user 
+   * @param Simple\Core\Request $request the request object
+   * @return bool
+   */
+  public static function authorize(Request $request)
+  {
+    $path = $request->getPath();
+    $groupId = Session::get('groupId');
 
-        if (!$password) {
-            exit('!! EMPTY PASSWORD NOT ALLOWED !!');
-        }
+    $userType = explode('/', $path)[0];
 
-        $user = new UserModel($userName, $password);
-        $data = $user->login($userName, $password);
+    $groupType = self::$usersTypes[$groupId];
 
-        if ($data) {
-            foreach ($data as $key => $value) {
-                Session::start();
-                Session::set($key, $value);
-            }
-            Session::set('userType', self::$usersTypes[$data['groupId']]);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Authorize the logged in user 
-     * @param Simple\Core\Request $request the request object
-     * @return bool
-     */
-    public static function authorize(Request $request)
-    {
-        $path = $request->getPath();
-        $groupId = Session::get('groupId');
-
-        $userType = explode('/', $path)[0];
-
-        $groupType = self::$usersTypes[$groupId];
-
-        return $groupType === $userType;
-    }
+    return $groupType === $userType;
+  }
 }
