839aa9535100164880ed3f994efad118c7dc4799
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-11-25 20:59:36 +0200

    enable passing params to the script
    passing parameters to the script like watch, comperss

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -1,4 +1,16 @@
+param($watch, $compress)
+
 $source = "assets\main.scss"
 $dest = "public\css\style.css"
+$command = "sass " + $source + " " + $dest + " "
 
-sass $source $dest
+
+if ($watch -eq "watch") {
+  $command += "--watch "
+}
+
+if ($compress -eq "compress") {
+  $command += "--style compressed "
+}
+
+Invoke-Expression $command
