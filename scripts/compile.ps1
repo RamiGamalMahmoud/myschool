5648d12352c84b8634457e97fe57ac66b9b6ab70
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-11-28 21:00:16 +0200

    make sass compilation watch and compressed

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -1,16 +1,11 @@
-param($watch, $compress)
 
 $source = "assets\main.scss"
 $dest = "public\css\style.css"
 $command = "sass " + $source + " " + $dest + " "
 
 
-if ($watch -eq "watch") {
   $command += "--watch "
-}
 
-if ($compress -eq "compress") {
   $command += "--style compressed "
-}
 
 Invoke-Expression $command
