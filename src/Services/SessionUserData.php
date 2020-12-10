625b3b4ac92e0c6baa4cc4ee0f760fc677844d6b
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-12-10 21:57:50 +0200

    Add Services/SessionUserData.php
    A service that responsable of getting the current user data like
    userName, userGroup

---
+++ src/Services/SessionUserData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SM\Services;
+
+use Simple\Core\Session;
+
+/**
+ * Manage Users data in local storage [sessions, cockies, local storage]
+ */
+class SessionUserData
+{
+  /**
+   * Get the user name from sessions
+   * @return string ther user name
+   */
+  public function getUserName()
+  {
+    Session::start();
+    return Session::get('fullName');
+  }
+}
