fd724d4cb8aa4e8884c748196d36089c72d9cbfb
rami-gamal <rami.gamal.mahmoud@gmail.com>
2020-12-30 20:31:20 +0200

    Initialize Simple\Core\View

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
 use Simple\Core\Simple;
+use Simple\Core\View;
 
 require_once('../config/config.php');
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+View::init(VIEWS_PATH, COMPILE_PATH, VIEWS_AUTO_RELOAD);
 
 $app = new Simple(ROUTES_FOLDER);
 
