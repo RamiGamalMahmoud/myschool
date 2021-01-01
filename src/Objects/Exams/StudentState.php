260eef4f267ad2ec32f3d419dd1d05da94adcaa5
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-01 23:58:01 +0200

    Check if all subjects is assigned

--- src/Objects/Exams/StudentState.php
+++ src/Objects/Exams/StudentState.php
@@ -4,11 +4,26 @@ namespace SM\Objects\Exams;
 
 class StudentState
 {
-    private string $_studentState;
-    private array $_weaknessSubjects;
+    private ?string $_studentState;
+    private ?array $_weaknessSubjects;
+    private bool $_isFullAssigned;
 
     public function __construct($semester, array $subjects)
     {
+        $assigned = array_filter($subjects, function ($subject) {
+            if ($subject->getGrade()->isAssigned()) {
+                return $subject;
+            }
+        });
+
+        $this->_isFullAssigned = count($assigned) === count($subjects);
+
+        if ($this->_isFullAssigned !== true) {
+            $this->_studentState = null;
+            $this->_weaknessSubjects = null;
+            return;
+        }
+
         $this->_weaknessSubjects = array_filter($subjects, function ($subject) {
             if ($subject->getGrade()->grade() === 'F' || $subject->getGrade()->isAbsence()) {
                 return $subject;
