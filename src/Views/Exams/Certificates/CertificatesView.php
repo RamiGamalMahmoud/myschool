65553ff4d430857c2c7e6319bbe33a349140b910
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-04 14:19:43 +0200

    Add CertificatesView

---
+++ src/Views/Exams/Certificates/CertificatesView.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SM\Views\Exams\Certificates;
+
+use Exception;
+use Simple\Core\View;
+use SM\Views\Helpers\Grade;
+use SM\Views\Helpers\Semester;
+
+class CertificatesView
+{
+    private string $template;
+    private ?array $context;
+
+    public function __construct(string $semester, $gradeNumber)
+    {
+        $certificateHeader = [
+            'semester' => Semester::semesterName($semester),
+            'gradeName' => Grade::gradeName($gradeNumber),
+            'schoolYear' => '2020 / 2021'
+        ];
+
+        $this->context['certificateHeader'] = $certificateHeader;
+        if ($semester === 'fs' || $semester === 'ss') {
+            $this->template = 'exams/certificates/certificates.twig';
+        } else {
+            throw new Exception('Semester Error');
+        }
+    }
+
+    public function load(array $context)
+    {
+        $this->setEntities($context);
+        return View::load($this->template, $this->context);
+    }
+
+    public function setEntities(array $entities)
+    {
+        $data = array_map(function ($item) {
+            return $item->toArray();
+        }, $entities);
+        $this->context['entities'] = $data;
+    }
+}
