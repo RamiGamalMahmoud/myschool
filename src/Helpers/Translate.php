057089003cb23556db0665b924192c8063b5ca96
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-07 15:47:07 +0200

    Add getSubjectName function

--- src/Helpers/Translate.php
+++ src/Helpers/Translate.php
@@ -4,4 +4,15 @@ namespace SM\Helpers;
 
 class Translator
 {
+    private static array $local;
+
+    public static function init(array $local)
+    {
+        self::$local = $local;
+    }
+
+    public static function getSubjectName(string $subjectName): string
+    {
+        return self::$local[$subjectName];
+    }
 }
