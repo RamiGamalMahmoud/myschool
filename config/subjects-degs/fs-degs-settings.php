5d72890bde6e6dba783748f9d2666dc296d524ce
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-07 15:50:59 +0200

    Add second semester percent degrees

--- config/subjects-degs/fs-degs-settings.php
+++ config/subjects-degs/fs-degs-settings.php
@@ -1,32 +1,31 @@
 <?php
 
 return [
-    "arabic"     => ["evaluation" => 30, "written" => 70, "fs-percent" => 40, "ss-percent" => 80],
+    "arabic"     => ["evaluation" => 30, "written" => 70, "written-success" => 21, "fs-percent" => 40, "ss-percent" => 80],
 
-    "english"    => ["evaluation" => 30, "written" => 70, "fs-percent" => 30, "ss-percent" => 60],
+    "english"    => ["evaluation" => 30, "written" => 70, "written-success" => 21, "fs-percent" => 30, "ss-percent" => 60],
 
-    "socials"    => ["evaluation" => 30, "written" => 70, "fs-percent" => 20, "ss-percent" => 40],
+    "socials"    => ["evaluation" => 30, "written" => 70, "written-success" => 21, "fs-percent" => 20, "ss-percent" => 40],
 
     "math"       => [
-        "evaluation" => 30, "aljebra" => 35, "geometry" => 35, "fs-percent" => 30, "ss-percent" => 60
+        "evaluation" => 30, "aljebra" => 35, "geometry" => 35, "written-success" => 21, "fs-percent" => 30, "ss-percent" => 60
     ],
 
     "sciences"   => [
-        "evaluation" => 30, "practical" => 14, "written-exam" => 56, "fs-percent" => 20,
-
-        "ss-percent" => 40
+        "evaluation" => 30, "practical" => 14, "written-exam" => 56, "written-success" => 21,
+        "fs-percent" => 20, "ss-percent" => 40
     ],
 
     "activities" => ["evaluation" => 100, "fs-percent" => 10, "ss-percent" => 20],
 
-    "religion"   => ["evaluation" => 30, "written" => 70, "fs-percent" => 20, "ss-percent" => 40],
+    "religion"   => ["evaluation" => 30, "written" => 70, "written-success" => 21, "fs-percent" => 20, "ss-percent" => 40],
 
     "computer"   => [
-        "evaluation" => 30, "practical" => 14, "written-exam" => 56, "fs-percent" => 10,
-        "ss-percent" => 20
+        "evaluation" => 30, "practical" => 14, "written-exam" => 56, "written-success" => 21,
+        "fs-percent" => 10, "ss-percent" => 20
     ],
 
-    "draw"       => ["evaluation" => 30, "written" => 70, "fs-percent" => 10, "ss-percent" => 20],
+    "draw"       => ["evaluation" => 30, "written" => 70, "written-success" => 21, "fs-percent" => 10, "ss-percent" => 20],
 
     "sports"     => ["evaluation" => 20, "fs-percent" => 100, "ss-percent" => 100]
 ];
