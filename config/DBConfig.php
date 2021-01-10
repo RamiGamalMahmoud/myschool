cfcfe147046ba5397034b325b1cc8204ada96691
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-10 14:22:13 +0200

    Use myschool database

--- config/DBConfig.php
+++ config/DBConfig.php
@@ -6,35 +6,35 @@ use Simple\Core\DataAccess\IDBConfig;
 
 class DBConfig implements IDBConfig
 {
-  private const DB_NAME   = 'school_management';
-  private const DRIVER   = 'mysql';
-  private const HOST     = 'localhost';
-  private const PASSWORD = '1234';
-  private const PORT     = 3306;
-  private const USER_NAME = 'rami';
+    private const DB_NAME   = 'myschool';
+    private const DRIVER   = 'mysql';
+    private const HOST     = 'localhost';
+    private const PASSWORD = '';
+    private const PORT     = 3306;
+    private const USER_NAME = 'root';
 
-  public static function getConnectionString(): string
-  {
-    return self::DRIVER . ':host=' . self::HOST . ';dbname=' . self::DB_NAME;
-  }
+    public static function getConnectionString(): string
+    {
+        return self::DRIVER . ':host=' . self::HOST . ';dbname=' . self::DB_NAME;
+    }
 
-  public static function getOptinos(): array
-  {
-    return [];
-  }
+    public static function getOptinos(): array
+    {
+        return [];
+    }
 
-  public static function getUserName(): string
-  {
-    return self::USER_NAME;
-  }
+    public static function getUserName(): string
+    {
+        return self::USER_NAME;
+    }
 
-  public static function getPassword(): string
-  {
-    return self::PASSWORD;
-  }
+    public static function getPassword(): string
+    {
+        return self::PASSWORD;
+    }
 
-  public static function getPort(): int
-  {
-    return self::PORT;
-  }
+    public static function getPort(): int
+    {
+        return self::PORT;
+    }
 }
