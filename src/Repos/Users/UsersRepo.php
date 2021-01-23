7809fce7a7a115634eb2309d101276f8372e5e5f
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-23 23:06:23 +0200

    Use User class

--- src/Repos/Users/UsersRepo.php
+++ src/Repos/Users/UsersRepo.php
@@ -6,6 +6,7 @@ use Simple\Core\DataAccess\IDataAccess;
 use SM\Repos\IRepo;
 use Simple\Core\DataAccess\Query;
 use SM\Entities\Entity;
+use SM\Entities\Users\User;
 
 class UsersRepo implements IRepo
 {
@@ -22,7 +23,7 @@ class UsersRepo implements IRepo
     public function getAll()
     {
         $query = new Query();
-        $query->select(['userName', 'userId', 'fullName', 'groupId'])
+        $query->select(['id', 'user_name', 'full_name', 'group_id'])
             ->from('users');
         return $this->dataAccess->getAll($query);
     }
@@ -39,13 +40,24 @@ class UsersRepo implements IRepo
     {
     }
 
-    public function auth(string $userName, string $password)
+    /**
+     * Get the logged user from database
+     * 
+     * @param string $userName
+     * @param string $password
+     * @return \SM\Entities\Users\User
+     */
+    public function getByNameAndPassword(string $userName, string $password): ?User
     {
         $query = new Query();
-        $query->select(['userId', 'userName', 'fullName', 'groupId'])
+        $query->select(['id', 'user_name', 'full_name', 'group_id'])
             ->from('users')
-            ->where('userName', '=', $userName)
-            ->andWhere('password', '=', $password);
-        return $this->dataAccess->get($query);
+            ->where('user_name', '=', $userName)
+            ->andWhere('password', '=', $password)
+            ->limit(1);
+        $data = $this->dataAccess->get($query);
+        if ($data !== false)
+            return new User($data);
+        return null;
     }
 }
