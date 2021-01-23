f1cca9e43a463ae7dd6fa67bae4877e9f8ede44e
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-23 23:04:55 +0200

    Add functionality to the User class

--- src/Entities/Users/User.php
+++ src/Entities/Users/User.php
@@ -1,11 +1,39 @@
 <?php
 
-namespace SM\Entities;
+namespace SM\Entities\Users;
 
 class User
 {
-    public $userId;
-    public $userName;
-    public $fullName;
-    public $groupeId;
+
+    private $id;
+    private string $userName;
+    private string $fullName;
+    private int $groupId;
+
+    public function __construct(array $data)
+    {
+        $this->id = $data['id'];
+        $this->userName = $data['user_name'];
+        $this->fullName = $data['full_name'];
+        $this->groupId = $data['group_id'];
+    }
+    public function getUserId()
+    {
+        return $this->id;
+    }
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    public function getGroupId()
+    {
+        return $this->groupId;
+    }
 }
