801acc6b07374f5620b8b333a7d6092e3e521962
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-25 23:18:12 +0200

    Implement different error methodes

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -3,14 +3,48 @@
 namespace SM\Controllers;
 
 use Simple\Core\View;
-use stdClass;
 
 class ErrorController
 {
-    public static function error()
+    private static array $context;
+    private static string $template = 'errors/error.twig';
+
+    public static function render()
     {
+        View::render(self::$template, self::$context);
+    }
+
+    public static function internalError()
+    {
+        self::$context = [
+            'code' => 500,
+            'message' => 'internal server error',
+            'description' => 'we are sorry, this is an internal server error'
+        ];
+        http_response_code(500);
+        self::render();
+    }
+
+    public static function pageNotFound()
+    {
+        self::$context = [
+            'code' => 404,
+            'message' => 'page not found',
+            'description' => 'you are trying to visit page that not exist'
+        ];
         http_response_code(404);
-        View::render('errors/error.twig');
+        self::render();
+    }
+
+    public static function resourcesRemoved()
+    {
+        self::$context = [
+            'code' => 410,
+            'message' => 'page not found',
+            'description' => 'sorry, the resources you requested has been deleted'
+        ];
+        http_response_code(410);
+        self::render();
     }
 
     public static function api()
