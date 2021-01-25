3b7de89a42e7002940f270012e2bda2efbae71df
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-25 23:09:00 +0200

    Indent file

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
 let inputs = document.querySelectorAll('.login-form input:not(:last-child)');
 
-inputs.forEach((input) =>{
-    
-    input.addEventListener('focus', ()=>{
-        input.setAttribute('text-data', input.getAttribute('placeholder'));
-        input.setAttribute('placeholder', '');
-    });
+inputs.forEach((input) => {
 
-    input.addEventListener('blur', () =>{
-        input.setAttribute('placeholder', input.getAttribute('text-data'));
-    });
+  input.addEventListener('focus', () => {
+    input.setAttribute('text-data', input.getAttribute('placeholder'));
+    input.setAttribute('placeholder', '');
+  });
+
+  input.addEventListener('blur', () => {
+    input.setAttribute('placeholder', input.getAttribute('text-data'));
+  });
 });
