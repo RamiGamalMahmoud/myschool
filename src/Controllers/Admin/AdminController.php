1919bc4adc103cc3abc23ccee6c13452296d82e4
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-26 16:22:08 +0200

    Remove old code

--- src/Controllers/Admin/AdminController.php
+++ src/Controllers/Admin/AdminController.php
@@ -2,22 +2,29 @@
 
 namespace SM\Controllers\Admin;
 
-use Simple\Core\IRequest;
-use Simple\Core\Session;
-use Simple\Core\Simple;
 use Simple\Core\View;
-use Simple\EXceptions\RoutingException;
-use SM\Controllers\BaseController;
+use Simple\Core\Router;
+use Simple\Core\Request;
+use Simple\Core\Session;
 
-class AdminController extends BaseController
+class AdminController
 {
+    private string $template = 'admin/admin.twig';
 
-    public function __construct(IRequest $request)
+    private View $view;
+
+    private array $context = [];
+
+    private Request $request;
+
+    private Router $router;
+
+    public function __construct(Request $request, Router $router)
     {
-        Session::start();
-        parent::__construct($request, []);
-        $this->context['fullName'] = Session::get('fullName');
-        $this->view = 'admin/admin.twig';
+        $this->request = $request;
+        $this->router = $router;
+        $this->context['fullName'] = Session::get('user-full-name');
+        $this->template = 'admin/admin.twig';
     }
     public function index()
     {
@@ -26,7 +33,7 @@ class AdminController extends BaseController
 
     public function render($context)
     {
-        View::render($this->view, $context);
+        View::render($this->template, $context);
     }
 
     public function showUsers()
@@ -35,21 +42,5 @@ class AdminController extends BaseController
         $usersData = $this->model->read();
         $this->context['data'] = $usersData;
         $this->render($this->context);
-    }
-
-    public function reRoute()
-    {
-        $parentContext = [
-            'fullName' => $this->context['fullName'],
-            'parentTemplate' => $this->view,
-            'linkPrefex' => '/admin'
-        ];
-
-        $newPath = explode('/', $this->request->getPath(), 2)[1];
-        try {
-            Simple::resolve($newPath, $parentContext);
-        } catch (RoutingException $e) {
-        }
-        return;
     }
 }
