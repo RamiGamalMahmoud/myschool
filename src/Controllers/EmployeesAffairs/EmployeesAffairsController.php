c286607b9032dd079e19c05b57e99f099f31588e
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-30 17:12:13 +0200

    Add EmployeesAffairsController class

--- src/Controllers/EmployeesAffairs/EmployeesAffairsController.php
+++ src/Controllers/EmployeesAffairs/EmployeesAffairsController.php
@@ -2,9 +2,40 @@
 
 namespace SM\Controllers\EmployeesAffairs;
 
+use Simple\Core\DataAccess\MySQLAccess;
+use Simple\Core\Request;
+use Simple\Core\Router;
+use Simple\Helpers\Log;
+use SM\Repos\EmployeesAffairs\EmployeeRepo;
+use SM\Repos\EmployeesAffairs\EmployeeRepoInterface;
+
 class EmployeesAffairsController
 {
-    public function index()
+    /**
+     * @var \Simple\Core\Request
+     */
+    private Request $request;
+
+    /**
+     * @var \Simple\Core\Router
+     */
+    private Router $router;
+
+    /**
+     * @var \SM\Repos\EmployeesAffairs\EmployeeRepoInterface
+     */
+    private EmployeeRepoInterface $employeeRepo;
+
+    /**
+     * Construstor
+     * 
+     * @param \Simple\Core\Request $request
+     * @param \Simple\Core\Router $router
+     */
+    public function __construct(Request $request, Router $router)
     {
+        $this->request = $request;
+        $this->router = $router;
+        $this->employeeRepo = new EmployeeRepo(new MySQLAccess());
     }
 }
