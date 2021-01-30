6c34c22f424932c0596e906b71e6459667a56a69
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-01-30 17:21:01 +0200

    Implement Class code
    - Add class variables
    - Add the constructor
    - Add showMainView method

--- src/Views/EmployeesAffairs/EmployeesAffairsView.php
+++ src/Views/EmployeesAffairs/EmployeesAffairsView.php
@@ -2,6 +2,40 @@
 
 namespace SM\Views\EmployeesAffairs;
 
+use Simple\Core\View;
+
 class EmployeesAffairsView
 {
+    /**
+     * @var string $mainTemplate
+     */
+    private string $mainTemplate;
+
+    /**
+     * @var string $editTemplate
+     */
+    private string $editTemplate;
+
+    /**
+     * @var array $contextData
+     */
+    private string $contextData;
+
+    /**
+     * Constructor
+     * 
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        $this->contextData = $data;
+    }
+
+    /**
+     * Rndering the main view
+     */
+    public function mainView()
+    {
+        View::render($this->mainTemplate, $this->contextData);
+    }
 }
