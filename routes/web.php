1af69ff3cca03be805d4bedae1dd0f6f946c6abd
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-02-02 20:57:58 +0200

    Remove commented lines

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,11 @@ use SM\Controllers\LoginController;
 use SM\Controllers\Users\UserController;
 use SM\MiddleWares\Auth;
 
-/**
- * HTTP GET METHOD ROUTES
- */
-
 Route::get('login', [LoginController::class, 'showLogin']);
 
 Route::get('logout', [Logout::class, 'logout']);
 
 Route::get('/', [IndexController::class, 'index'], ['isAuthenticated']);
-
-/**
- * ADMIN HTTP GET ROUTES
- */
 
 Route::get('admin', [AdminController::class, 'index'], ['isAuthenticated', 'authorize']);
 
@@ -37,19 +29,11 @@ Route::get('admin/users/delete/{id}', [UserController::class, 'remove']);
 
 Route::get('admin/users/block/{id}', [UserController::class, 'blockUser']);
 
-/**
- * EMPLOYEES_AFFAIRS GET ROUTES
- */
-
 Route::get('employees-affairs', [EmployeesAffairsController::class, 'index']);
 
 Route::get('employees-affairs/show-all/{criteria}/{value}', [EmployeesAffairsController::class, 'getBy']);
 
 Route::get('employees-affairs/edit/{id}', [EmployeesAffairsController::class, 'edit']);
-
-/**
- * EXAMS HTTP GET ROUTES
- */
 
 Route::get('exams', [ExamsController::class, 'index']);
 
@@ -61,40 +45,14 @@ Route::get('sheets/{gradeNumber}/{semester}/{status}', [SheetsController::class,
 
 Route::get('certificates/{gradeNumber}/{semester}/{status}',  [CertificatesController::class, 'index']);
 
-/**
- * HTTP POST METHOD ROUTES
- */
-
 Route::post('login', [LoginController::class, 'doLogin']);
-
-/**
- * ADMIN ROUTES
- */
 
 Route::post('admin/users/edit/{id}', [UserController::class, 'update']);
 
-/**
- * EMPLOYEES ROUTES
- */
 Route::post('employees-affairs/edit/{id}', [EmployeesAffairsController::class, 'update']);
 
-### Exams Routes
-
 Route::post('monitoring/{gradeNumber}/{monitoringType}/{semester}', [MonitoringController::class, 'store']);
-
-/**
- * MIDDLEWARES ROUTES
- */
 
 Route::middleware('isAuthenticated', [Auth::class, 'isAuthenticated']);
 
 Route::middleware('authorize', [Auth::class, 'authorize']);
-
-/**
- * CALLABLE ROUTES
- */
-
-
-/**
- * VIEWS ROUTES
- */
