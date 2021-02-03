14a596c4959cbe520b12a10f1a9f45e86f788b65
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-02-03 16:36:58 +0200

    Add TranslateEmployeeData

---
+++ src/Services/TranslateEmployeeData.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SM\Services;
+
+use SM\Helpers\Translate;
+
+class TranslateEmployeeData
+{
+    public static function translateEmployee($employee)
+    {
+        $translateData = [];
+        $translateData['id'] = $employee['id'];
+        $translateData['personal-data'] = self::translatePersonalData($employee['personal-data']);
+        $translateData['social-data'] = self::translateSocialData($employee['social-data']);
+        $translateData['job-data'] = self::translateJobData($employee['job-data']);
+        $translateData['address'] = $employee['address'];
+        return $translateData;
+    }
+
+    public static function translatePersonalData(array $personalData): array
+    {
+        $translateData  = [
+            'name' => $personalData['name'],
+            'national-id' => Translate::convertNumbers($personalData['national-id']),
+            'pirthdate' => Translate::convertDate($personalData['pirthdate']),
+            'gender' => ['key' => $personalData['gender'], 'value' => Translate::get('gender', $personalData['gender'])],
+            'religion' => ['key' => $personalData['religion'], 'value' => Translate::get('religion', $personalData['religion'])],
+            'nationality' => ['key' => $personalData['nationality'], 'value' => Translate::get('nationality', $personalData['nationality'])]
+        ];
+        return $translateData;
+    }
+
+    public static function translateSocialData(array $socialData): array
+    {
+        $translateData = [
+            'martial-status' => ['key' => $socialData['martial-status'], 'value' => Translate::get('martial-status', $socialData['martial-status'])],
+            'children-count' => Translate::convertNumbers($socialData['children-count'])
+        ];
+        return $translateData;
+    }
+
+    public static function translateJobData(array $jobData): array
+    {
+        $translateData = [
+            'date-of-hiring' => Translate::convertDate($jobData['date-of-hiring']),
+            'date-of-work-received' => Translate::convertDate($jobData['date-of-work-received']),
+            'employee-type' => ['key' => $jobData['employee-type'], 'value' => Translate::get('job-data.employee-type', $jobData['employee-type'])],
+            'employee-status' => ['key' => $jobData['employee-status'], 'value' => Translate::get('job-data.employee-status', $jobData['employee-status'])],
+            'attitude-to-work' => ['key' => $jobData['attitude-to-work'], 'value' => Translate::get('job-data.attitude-to-work', $jobData['attitude-to-work'])]
+        ];
+        return $translateData;
+    }
+}
