d255cbd9eea967c14dd9f8275bea93d0024fbfa9
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-02-05 11:57:27 +0200

    Add columns file

---
+++ src/Repos/EmployeesAffairs/employee-table-columns.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'id',
+    'name',
+    'national_id',
+    'pirthdate',
+    'gender',
+    'religion',
+    'nationality',
+    'employee_type',
+    'attitude_to_work',
+    'employee_status',
+    'date_of_hiring',
+    'date_of_work_received',
+    'martial_status',
+    'children_count',
+    'fixed_phone',
+    'mobile',
+    'governorate_name',
+    'governorate_id ',
+    'city_name',
+    'city_id ',
+    'district',
+    'street',
+    'code',
+    'insurance_number'
+];
