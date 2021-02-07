509aa64338597e907c50f8da773b8aa9eac96fa9
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-02-07 22:48:43 +0200

    Checks authentation

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -2,12 +2,18 @@
 
 namespace SM\Controllers;
 
+use Simple\Core\Redirect;
 use Simple\Core\Session;
+use SM\MiddleWares\Auth;
 
 class IndexController
 {
     public function index()
     {
+        if (!Auth::isAuthenticated()) {
+            Redirect::to('/login');
+        }
+
         $location = Session::get('group-name');
         header('location: /' . $location);
     }
