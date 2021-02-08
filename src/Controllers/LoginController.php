9591cb5cf99527ffe5740f6c152c47363dd5f69d
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-02-08 22:54:56 +0200

    Use Request and Router

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -4,20 +4,30 @@ namespace SM\Controllers;
 
 use Simple\Core\Redirect;
 use Simple\Core\Request;
+use Simple\Core\Router;
 use Simple\Core\Session;
 use SM\MiddleWares\Auth;
 use Simple\Core\View;
 
 class LoginController
 {
+    private Request $request;
+
+    private Router $router;
+
+    public function __construct(Request $request, Router $router)
+    {
+        $this->request = $request;
+        $this->router = $router;
+    }
     public function showLogin()
     {
         View::render('login.twig');
     }
 
-    public function doLogin(Request $request)
+    public function doLogin()
     {
-        $params = $request->getRequestBody()['post'];
+        $params = $this->request->getRequestBody()['post'];
         $userName = isset($params['userName']) && !empty($params['userName']) ? $params['userName'] : false;
         $password = isset($params['password']) && !empty($params['password']) ? $params['password'] : false;
 
