5aa0052959a95c828fb98870696a58670d473b7f
rami-gamal <rami.gamal.mahmoud@gmail.com>
2021-02-09 16:48:06 +0200

    add translate functions

--- src/Services/TranslateEmployeeData.php
+++ src/Services/TranslateEmployeeData.php
@@ -2,6 +2,8 @@
 
 namespace SM\Services;
 
+use SM\Entities\EmployeesAffairs\AttitudeToWork;
+use SM\Entities\EmployeesAffairs\MartialStatus;
 use SM\Helpers\Translate;
 
 class TranslateEmployeeData
@@ -32,9 +34,18 @@ class TranslateEmployeeData
             'name' => $personalData['name'],
             'national-id' => Translate::convertNumbers($personalData['national-id']),
             'pirthdate' => Translate::convertDate($personalData['pirthdate']),
-            'gender' => ['key' => $personalData['gender'], 'value' => Translate::get('gender', $personalData['gender'])],
-            'religion' => ['key' => $personalData['religion'], 'value' => Translate::get('religion', $personalData['religion'])],
-            'nationality' => ['key' => $personalData['nationality'], 'value' => Translate::get('nationality', $personalData['nationality'])]
+            'gender' => [
+                'key' => $personalData['gender'],
+                'value' => Translate::get('gender', $personalData['gender'])
+            ],
+            'religion' => [
+                'key' => $personalData['religion'],
+                'value' => Translate::get('religion', $personalData['religion'])
+            ],
+            'nationality' => [
+                'key' => $personalData['nationality'],
+                'value' => Translate::get('nationality', $personalData['nationality'])
+            ]
         ];
         return $translateData;
     }
@@ -42,7 +53,10 @@ class TranslateEmployeeData
     public static function translateSocialData(array $socialData): array
     {
         $translateData = [
-            'martial-status' => ['key' => $socialData['martial-status'], 'value' => Translate::get('martial-status', $socialData['martial-status'])],
+            'martial-status' => [
+                'key' => $socialData['martial-status'],
+                'value' => Translate::get('martial-status', $socialData['martial-status'])
+            ],
             'children-count' => Translate::convertNumbers($socialData['children-count'])
         ];
         return $translateData;
@@ -53,10 +67,37 @@ class TranslateEmployeeData
         $translateData = [
             'date-of-hiring' => Translate::convertDate($jobData['date-of-hiring']),
             'date-of-work-received' => Translate::convertDate($jobData['date-of-work-received']),
-            'employee-type' => ['key' => $jobData['employee-type'], 'value' => Translate::get('job-data.employee-type', $jobData['employee-type'])],
-            'employee-status' => ['key' => $jobData['employee-status'], 'value' => Translate::get('job-data.employee-status', $jobData['employee-status'])],
-            'attitude-to-work' => ['key' => $jobData['attitude-to-work'], 'value' => Translate::get('job-data.attitude-to-work', $jobData['attitude-to-work'])]
+            'employee-type' => [
+                'key' => $jobData['employee-type'],
+                'value' => Translate::get('job-data.employee-type', $jobData['employee-type'])
+            ],
+            'employee-status' => [
+                'key' => $jobData['employee-status'],
+                'value' => Translate::get('job-data.employee-status', $jobData['employee-status'])
+            ],
+            'attitude-to-work' => [
+                'key' => $jobData['attitude-to-work'],
+                'value' => Translate::get('job-data.attitude-to-work', $jobData['attitude-to-work'])
+            ]
         ];
         return $translateData;
+    }
+
+    public static function translateMartialStatus(MartialStatus $martialStatus): array
+    {
+        return [
+            'id' => $martialStatus->getId(),
+            'key' => $martialStatus->getMartialStatus(),
+            'value' => Translate::get('martial-status', $martialStatus->getMartialStatus())
+        ];
+    }
+
+    public static function translateAttitudeToWork(AttitudeToWork $attitudeToWork): array
+    {
+        return [
+            'id' => $attitudeToWork->getId(),
+            'key' => $attitudeToWork->getAttitude(),
+            'value' => Translate::get('job-data.attitude-to-work', $attitudeToWork->getAttitude())
+        ];
     }
 }
