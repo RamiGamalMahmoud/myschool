ab84e57920e91b75aa1853445e542bdbb8da7d4b
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-02-15 21:02:59 +0200

    show reset-btn

--- assets/js/employees/employees.js
+++ assets/js/employees/employees.js
@@ -37,3 +37,31 @@ function getCitiesByGovernorateId(governorateId) {
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.send();
 }
+
+const formParts = {
+  presonalPart: document.getElementById('presonal-data'),
+  employeeStatus: document.getElementById('employee-status'),
+  socialStatus: document.getElementById('social-status'),
+  address: document.getElementById('address'),
+  phone: document.getElementById('phone')
+};
+
+for (const fieldSet in formParts) {
+  let selectElements = formParts[fieldSet].querySelectorAll('select');
+  let resetButton = formParts[fieldSet].querySelector('.reset-btn');
+
+  for (let i = 0; i < selectElements.length; i++) {
+    selectElements[i].addEventListener('input', (ev) => {
+      if (resetButton !== null)
+        resetButton.style.visibility = 'visible';
+    });
+  }
+}
+
+const resetButtons = document.querySelectorAll('.reset-btn');
+
+for (const resetButton of resetButtons) {
+  resetButton.addEventListener('click', (ev) => {
+    ev.preventDefault();
+  });
+}
