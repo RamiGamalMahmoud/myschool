8f711408264733600e8963c0aca5b8ee78a66172
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-02-17 16:33:54 +0200

    use new classes

--- assets/js/src/employees/reset-employee-form.js
+++ assets/js/src/employees/reset-employee-form.js
@@ -1,34 +1,15 @@
+import { Resetable, ResetableFieldset } from '../../lib';
 
 export default function resetEmployeeForm() {
   const employeeForm = document.getElementById('edit-employee-form');
   if (employeeForm === null) {
     return;
   }
-  const formParts = {
-    presonalPart: document.getElementById('presonal-data'),
-    employeeStatus: document.getElementById('employee-status'),
-    socialStatus: document.getElementById('social-status'),
-    address: document.getElementById('address'),
-    phone: document.getElementById('phone')
-  };
-
-  for (const fieldSet in formParts) {
-    let selectElements = formParts[fieldSet].querySelectorAll('select');
-    let resetButton = formParts[fieldSet].querySelector('.reset-btn');
-
-    for (let i = 0; i < selectElements.length; i++) {
-      selectElements[i].addEventListener('input', (ev) => {
-        if (resetButton !== null)
-          resetButton.style.visibility = 'visible';
-      });
-    }
-  }
-
-  const resetButtons = document.querySelectorAll('.reset-btn');
-
-  for (const resetButton of resetButtons) {
-    resetButton.addEventListener('click', (ev) => {
-      ev.preventDefault();
-    });
-  }
+  const formParts = [
+    new ResetableFieldset(document.getElementById('presonal-data')),
+    new ResetableFieldset(document.getElementById('employee-status')),
+    new ResetableFieldset(document.getElementById('social-status')),
+    new ResetableFieldset(document.getElementById('address')),
+    new ResetableFieldset(document.getElementById('phone'))
+  ];
 }
