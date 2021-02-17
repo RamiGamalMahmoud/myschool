e0734f2c2af45898902f5d0a99a94dddc819496b
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-02-17 16:33:16 +0200

    add index.js
    exports classes in lib

---
+++ assets/js/lib/index.js
@@ -0,0 +1,9 @@
+import DataTable from './datatable';
+import Resetable from './resetable';
+import ResetableFieldset from './resetable-fieldset';
+
+export {
+  DataTable,
+  Resetable,
+  ResetableFieldset
+};
