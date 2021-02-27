03c3f434c942341a6c86084c827ca81acf4d82a5
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-02-27 17:17:53 +0200

    init datatables by default

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
+import { DataTable } from './lib';
+
+(function () {
+  let dataTables = document.querySelectorAll('.data-table');
+  if (dataTables) {
+    Array.from(dataTables).forEach((element) => {
+      let dataTable = new DataTable(element);
+      dataTable.init();
+    });
+  }
+})();
+
 import './src/employees';
+import './src/students-affairs';
 import './src/exams';
 import './src/login.js';
