ec2705afd437c4aaba15529c903d4a9b38f530a8
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-04-17 23:14:26 +0200

    add functions

--- src/Repos/Timetable/ITimetableRepo.php
+++ src/Repos/Timetable/ITimetableRepo.php
@@ -4,4 +4,12 @@ namespace SM\Repos\Timetable;
 
 interface ITimetableRepo
 {
+    public function getAllDays(): array;
+    public function getByDayId($dayId);
+    public function isTeacherAvailable($teacherId, $subjectId, $periodId, $dayId);
+    public function isClassroomAvailable($classroomId, $periodId, $dayId);
+    public function getTimetableId($teacherId, $usbjectId, $dayId, $periodId);
+    public function updateTimetable($id, $classroomId, $dayId, $periodId);
+    public function createTimeTable($dayId,  $periodId, $classroomId, $teacherClassroomsId);
+    public function deleteTimetable($id);
 }
--- src/Repos/Timetable/TimetableRepo.php
+++ src/Repos/Timetable/TimetableRepo.php
@@ -3,12 +3,126 @@
 namespace SM\Repos\Timetable;
 
 use Simple\Core\DataAccess\IDataAccess;
+use Simple\Core\DataAccess\Query;
 use SM\Repos\BaseRepo;
 
 class TimetableRepo extends BaseRepo implements ITimetableRepo
 {
     public function __construct(IDataAccess $dataAccess)
     {
-        parent::__construct($dataAccess, '');
+        parent::__construct($dataAccess, 'timetable');
+    }
+
+    public function getTimetableId($teacherId, $usbjectId, $dayId, $periodId)
+    {
+        $query = new Query();
+        $query->select(['id'])
+            ->from('timetable_view')
+            ->where('teacher_id', '=', $teacherId)
+            ->andWhere('subject_id', '=', $usbjectId)
+            ->andWhere('day_id', '=', $dayId)
+            ->andWhere('period_id', '=', $periodId);
+        $timetableId = $this->dataAccess->get($query);
+        if ($timetableId) {
+            return $timetableId['id'];
+        }
+        return false;
+    }
+
+    public function getAllDays(): array
+    {
+        $query = new Query();
+        $query->selectAll()->from('timetable_view');
+        return $this->groupByDay($this->dataAccess->getAll($query));
+    }
+
+    public function getByDayId($dayId)
+    {
+        $query = new Query();
+        $query->selectAll()->from('timetable_view')->where('day_id', '=', $dayId);
+        return $this->groupByTeachers($this->dataAccess->getAll($query));
+    }
+
+    public function groupByDay($data)
+    {
+        $timetableDays = array_reduce($data, function ($acc, $day) {
+            $className = explode('-', $day['class_name']);
+            $className = array_reverse($className);
+            $className = implode('-', $className);
+            $acc[$day['day_id']][$day['teacher_id']][$day['subject_id']][$day['period_id']] = $className;
+            return $acc;
+        }, []);
+        return $timetableDays;
+    }
+
+    public function groupByTeachers($data)
+    {
+        $timetableDays = array_reduce($data, function ($acc, $day) {
+            $className = explode('-', $day['class_name']);
+            $className = array_reverse($className);
+            $className = implode('-', $className);
+            $acc[$day['teacher_id']][$day['subject_id']][$day['period_id']] = $className;
+            return $acc;
+        }, []);
+        return $timetableDays;
+    }
+
+    public function updateTimetable($id, $classroomId, $dayId, $periodId)
+    {
+        $query = new Query();
+        $query->update('timetable')
+            ->set(['classroom_id' => $classroomId])
+            ->where('id', '=', $id);
+        return $this->dataAccess->run($query);
+    }
+
+    public function createTimeTable($dayId,  $periodId, $classroomId, $teacherClassroomsId)
+    {
+        $query = new Query();
+        $query->insertInto('timetable')
+            ->values(
+                [
+                    'id' => null,
+                    'day_id' => $dayId,
+                    'classroom_id' => $classroomId,
+                    'period_id' => $periodId,
+                    'teacher_classrooms_id' => $teacherClassroomsId
+                ]
+            );
+        return $this->dataAccess->run($query);
+    }
+
+    public function deleteTimetable($id)
+    {
+        $query = new Query();
+        $query->delete()
+            ->from('timetable')
+            ->where('id', '=', $id);
+        return $this->dataAccess->run($query);
+    }
+
+    public function isTeacherAvailable($teacherId, $usbjectId, $periodId, $dayId)
+    {
+        $query = new Query();
+        $query->selectCount('id')
+            ->from('timetable_view')
+            ->where('teacher_id', '=', $teacherId)
+            ->andWhere('subject_id', '!=', $usbjectId)
+            ->andWhere('period_id', '=', $periodId)
+            ->andWhere('day_id', '=', $dayId);
+        $result = $this->dataAccess->get($query);
+        return $result['count'] == 0;
+    }
+
+    public function isClassroomAvailable($classroomId, $periodId, $dayId)
+    {
+        $query = new Query();
+        $query->selectCount('id')
+            ->from('timetable_view')
+            ->where('classroom_id', '=', $classroomId)
+            ->andWhere('period_id', '=', $periodId)
+            ->andWhere('day_id', '=', $dayId);
+        $result = $this->dataAccess->get($query);
+        return $result['count'] == 0;
     }
 }
