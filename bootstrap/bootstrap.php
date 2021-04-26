4cc8ece5617be678d051e43defed26c6949c6bcf
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-04-26 21:25:01 +0200

    remove old code

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,28 +1,16 @@
 <?php
 
-use Simple\Core\View;
+use SM\Controllers\ErrorController;
+use Simple\Core\Application;
+use Simple\Helpers\Log;
 use SM\Helpers\Translate;
-use Simple\Helpers\Config;
-use Simple\Core\DataAccess\MySQLAccess;
-use Simple\Core\Router;
-
-require_once '../config/init.php';
-require_once '../config/config.php';
 
 require_once '../vendor/autoload.php';
 
-$database_config = require_once(DB_CONFIG);
-
-Config::load(CONFIG_DIR);
-
-Router::setRoutesDirectory(ROUTES_FOLDER);
-
-MySQLAccess::config($database_config[DATABASE_DRIVER]);
-
-MySQLAccess::connect();
-
 Translate::init(require_once '../resources/locals/ar.php');
 
-View::init(VIEWS_PATH, COMPILE_PATH, VIEWS_AUTO_RELOAD);
+$app = Application::getInstance(dirname(__DIR__));
 
-date_default_timezone_set(Config::get('app.time_zone'));
+$app->setErrorHandler(new ErrorController());
+
+$app->run(true);
