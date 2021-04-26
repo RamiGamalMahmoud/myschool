899a723c558f570fe587ed510cbe8de6e6e2c371
Rami Gamal <rami.gamal.mahmoud@gmail.com>
2021-04-26 21:26:06 +0200

    remove old code

--- public/index.php
+++ public/index.php
@@ -1,10 +1,3 @@
 <?php
 
-use Simple\Core\Application;
-use SM\Controllers\ErrorController;
-
 require_once '../bootstrap/bootstrap.php';
-
-Application::init(new ErrorController());
-
-Application::run();
